83269819d23045d51df202f647c50c802ff94bcd
accoffai <accoffai@gmail.com>
2025-07-03 14:30:45 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,21 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:fix_flow_a_i/main.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:mockito/mockito.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+// Mock Setup
+class MockFirebaseCore extends Mock
+    with MockPlatformInterfaceMixin
+    implements FirebasePlatform {}
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   setUpAll(() async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+    // Override the default Firebase implementation with a mock
+    Firebase.delegatePackingProperty = DefaultFirebasePlatform();
   });
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
