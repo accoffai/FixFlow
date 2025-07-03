f19b8b4a7cfb82766a34941fcd6b4e55b96f7049
accoffai <accoffai@gmail.com>
2025-07-03 14:15:49 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:fix_flow_a_i/main.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
