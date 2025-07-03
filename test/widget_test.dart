bfe0217a75004e2833c64be9ddc5bc6e677e18f1
accoffai <accoffai@gmail.com>
2025-07-03 14:41:08 -0400

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,25 +1,20 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:fix_flow_a_i/main.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:mockito/mockito.dart';
-import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-
-// Mock Setup
-class MockFirebaseCore extends Mock
-    with MockPlatformInterfaceMixin
-    implements FirebasePlatform {}
+import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
   setUpAll(() async {
-    // Override the default Firebase implementation with a mock
-    Firebase.delegatePackingProperty = DefaultFirebasePlatform();
+    // Mocks Firebase initialization
+    FirebasePlatform.instance = FakeFirebasePlatform();
+    await Firebase.initializeApp();
   });
 
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
@@ -31,3 +26,21 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+class FakeFirebasePlatform extends FirebasePlatform {
+  FakeFirebasePlatform() : super();
+
+  @override
+  Future<FirebaseAppPlatform> initializeApp({
+    required String name,
+    FirebaseOptions? options,
+  }) async {
+    return FakeFirebaseAppPlatform(name, options);
+  }
+}
+
+class FakeFirebaseAppPlatform extends FirebaseAppPlatform {
+  FakeFirebaseAppPlatform(String name, FirebaseOptions? options)
+      : super(name, options);
+}
+
